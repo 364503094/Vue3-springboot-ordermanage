c0786687c5f1979efc20b0cde7dcf546b80f62dd
unknown <364503094@qq.com>
2025-11-27 20:09:24 +0800

    First commit,完成登录页面LoginView.vue开发

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div>
+      <RouterView />
+    </div>
+</template>
+
+<style scoped>
+html,
+body,
+#app{
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  width:100vw;
+}
+</style>
